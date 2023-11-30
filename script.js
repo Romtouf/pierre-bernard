75232d42f5092875cbbc6d2389411f455826ed22
Romtouf <romtouf@laposte.net>
2023-11-30 11:14:43 +0100

    Mise en forme terminée exceptée le banner

--- script.js
+++ script.js
@@ -13,49 +13,25 @@ const slides = [
   },
 ];
 
-// Récupérer les éléments dans le HTML
 let arrowLeft = document.querySelector(".arrow_left");
 let arrowRight = document.querySelector(".arrow_right");
 let bannerImage = document.querySelector(".banner-img");
-// let bannerDots = document.querySelectorAll(".dot");
-// let bannerSelected = document.querySelector(".dot_selected");
 
-// Compteur pour le slide
 let affichageSlide = 0;
 
-// Au clic, faire défiler le carrousel
 arrowLeft.addEventListener("click", () => {
-  // alert("Ok pour la flèche de gauche");
-
   bannerContenu();
-  // dotChangeOnClick();
   bannerBoucle(-1);
 });
 arrowRight.addEventListener("click", () => {
-  // alert("Ok pour la flèche de droite");
-
   bannerContenu();
-  // dotChangeOnClick();
   bannerBoucle(1);
 });
 
-// Changer le point affiché au clic
-// function dotChangeOnClick() {
-//   bannerDots.forEach((bullets, i) => {
-//     bullets.classList.remove("dot_selected");
-
-//     if (i === affichageSlide) {
-//       bullets.classList.add("dot_selected");
-//     }
-//   });
-// }
-
-// Intégrer le contenu du slider
 function bannerContenu() {
   bannerImage.src = slides[affichageSlide].image;
 }
 
-// Sens de défilement du slide selon la flèche cliquée
 function bannerBoucle(sens) {
   affichageSlide = affichageSlide + sens;
   if (affichageSlide > slides.length - 1) {
